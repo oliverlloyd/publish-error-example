1a62dab54eae4dd9d974daa09a8fff604cc12895
Oliver Lloyd <oliver_lloyd@hotmail.com>
2014-06-12 21:25:17 +0100

    Spell questionnaire properly

--- client/views/questionnaire/questionnaire.js
+++ client/views/questionnaire/questionnaire.js
@@ -1,8 +1,8 @@
 
 
 Router.map(function() {
-  this.route('questionaire', {
-    path: '/projects/:_id/questionaire',
+  this.route('questionnaire', {
+    path: '/projects/:_id/questionnaire',
     waitOn: function () {
       return Meteor.subscribe('aProject', this.params._id);
     },
