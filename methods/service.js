a2c90abd0b0e5e24d2de1cd430cf74cd12c8aae6
Oliver Lloyd <oliver_lloyd@hotmail.com>
2014-06-15 00:34:19 +0100

    Added checks to addService function

--- methods/service.js
+++ methods/service.js
@@ -9,6 +9,10 @@
 
 Meteor.methods({
   addService: function(project, service) {
+    if ( service.hasOwnProperty('name') ) check(service.name, nonEmptyString);
+    if ( service.hasOwnProperty('type') ) check(service.type, nonEmptyString);
+    if ( service.hasOwnProperty('options') ) check(service.options, Array);
+
     if( allowedTo.updateProject(Meteor.user(), project) && isAcceptable(service) ){
       service.created = Date.now();
       Projects.update({'_id': project._id}, {$push: {services: service}, $set: {complete: true}});
@@ -23,6 +27,7 @@ var isAcceptable = function(service){
   check(service, {
     name: nonEmptyString,
     type: nonEmptyString,
+    options: Match.Optional(Array),
     _id: Match.Optional(nonEmptyString)
   });
 
@@ -30,6 +35,8 @@ var isAcceptable = function(service){
     throw new Meteor.Error(413, "Name too long");
   if ( service.name.length < 2 )
     throw new Meteor.Error(413, "Name too short, it should be greater than 2 characters");
+  if ( service.options && service.options.length < 1 )
+    throw new Meteor.Error(413, "No options, you must supply at least one option for a list");
   if (! Meteor.user())
     throw new Meteor.Error(403, "You must be logged in");
   else 
